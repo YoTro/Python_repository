079709a3d284b7eb3b01fae9ae875ac221591bb9
YoTro <zhongjin95@gmail.com>
2018-03-15 17:30:45 +0800

    Update Google_translate_js_API.py

--- Google_translate_js_API.py
+++ Google_translate_js_API.py
@@ -8,7 +8,7 @@ import requests
 import urllib
 import re
 class Py4Js():  
-  '''Google_translate js API'''    
+      
     def __init__(self):  
         self.ctx = execjs.compile(""" 
         function TL(a) { 
@@ -54,12 +54,13 @@ class Py4Js():
     def getTk(self,text):  
         return self.ctx.call("TL",text)
 def  zh_or_en(sl):
-    sl=sl.decode('unicode_escape')
+    s=sl.decode('GB2312')#The rules for translating a Unicode string into a sequence of bytes are called an encoding.
+                                                
     __zh=re.compile(u'[\u4e00-\u9fa5]+')
-    f=__zh.search(sl)
-    print sl
+    f=__zh.search(s)
     if f:#如果是中文，则用中译英URL
         q=urllib.quote(sl)
+        
         url="https://translate.google.cn/translate_a/single?client=t&sl=zh-CN&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=3&tsel=3&kc=3&"
     else:
         q=sl
@@ -72,6 +73,7 @@ print q
 tk=js.getTk(q)
 url=url+"tk={0}&q={1}".format(tk,q)
 r=requests.get(url)
-print r
+
+print  r.json()
     
     
