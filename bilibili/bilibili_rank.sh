9b5ca4bf1a27942432e93954edc40e69ddd46b92
YoTro <zhongjin95@gmail.com>
2020-04-21 16:59:04 +0800

    Update bilibili_rank.sh

--- bilibili/bilibili_rank.sh
+++ bilibili/bilibili_rank.sh
@@ -5,6 +5,6 @@ echo `date` >> /Users/jin/Documents/GitHub/Python_repository/bilibili/log &&
 # 进入helloworld.py程序所在目录
 cd /Users/jin/Documents/GitHub/Python_repository/bilibili &&
 # 执行python脚本（注意前面要指定python运行环境/usr/bin/python，根据自己的情况改变）
-/usr/bin/python bilibili_ranking_data_everyday.py
+python bilibili_ranking_data_everyday.py
 #运行完成
 echo 'finish' >> /Users/jin/Documents/GitHub/Python_repository/bilibili/log
