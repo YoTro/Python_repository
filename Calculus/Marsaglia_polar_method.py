8ff2e4b4e0b04eef6b21e94c8e6c2be3be0c8286
YoTro <zhongjin95@gmail.com>
2020-05-07 01:41:37 +0800

    mt19937
    梅森旋转算法伪随机数生成器

---
+++ Calculus/Marsaglia_polar_method.py
@@ -0,0 +1,29 @@
+﻿import MT19937
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def mt():
+    res = []
+    n = 10000
+    for i in xrange(n):
+        #生成624位0到1的均匀分布数字
+        res.append([math.sin(MT19937.MT19937(i).extract_number()), math.cos(MT19937.MT19937(i).extract_number())])
+    return res
+def Marsaglia_polar():
+    Gauss = []
+    res = mt()
+    for i in xrange(len(res)):
+        u = res[i][0]
+        v = res[i][1]
+        s = u**2 + v**2
+        if s < 1:
+            x = u*math.sqrt(-2*math.log(s)/s)
+            y = v*math.sqrt(-2*math.log(s)/s)
+            Gauss.append([x,y])
+    return Gauss
+if __name__ == '__main__':
+    r = Marsaglia_polar()
+    s = np.array(r).reshape(2,len(r))
+    plt.plot(s[0], s[1], 'r.')
+    plt.show()
