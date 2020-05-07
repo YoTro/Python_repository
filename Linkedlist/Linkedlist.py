574aa01a93f10a8468316850f95fa25a306027d6
YoTro <zhongjin95@gmail.com>
2020-05-07 22:13:30 +0800

    Update Linkedlist.py

--- Linkedlist/Linkedlist.py
+++ Linkedlist/Linkedlist.py
@@ -1,4 +1,4 @@
-class ListNode(object):
+﻿class ListNode(object):
  def __init__(self, x):
      self.val = x
      self.next = None
@@ -20,6 +20,56 @@ class Solution:
         for i in range(len(l)):
             self.head = self.add(self.head, l[i])
         return self.head
+    def delete(self, head, x):
+        if not head or not head.next:
+            return None
+        a = head
+        b = head.next
+        if x != head.val:
+            head.next = self.delete( head.next, x)
+        if b.val == x:
+            a.next = b.next
+        return head
+    def insert(self, index, x):
+        '''
+        index->int: 索引要插入的地方
+        x->int:     要插入的值
+        '''
+        if not self.head:
+            return None
+        node = self.search(index)
+        if node:
+            tmp = ListNode(x)
+            node.next = tmp
+            tmp.next = node.next.next
+            print("Insert success!")
+        return self.head
+    def search(self, x):
+        node = self.head
+        found = False
+        while not found and node:
+            if node.val == x:
+                print("We found this node in the linked list")
+                found = True
+                return node
+            else:
+                node = node.next
+        print("We can't found this node")
+    def update(self, index, x):
+        '''
+        index->int: original node
+        x->int:     new node
+        '''
+        if not self.head:
+            return None
+        node = self.search(index)
+        if node:
+            node.val = x
+            print("Update success!")
+        else:
+            return None
+        return node
+            
     def dfs(self, head):
         if head:
             res.append(head.val)
