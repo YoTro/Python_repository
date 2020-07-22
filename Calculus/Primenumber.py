70eb70e682fff77103354bb7c110b9e5a549d17c
YoTro <zhongjin95@gmail.com>
2020-07-22 11:15:41 +0800

    Update Primenumber.py

--- Calculus/Primenumber.py
+++ Calculus/Primenumber.py
@@ -5,12 +5,14 @@ import random
 def primeNumber(n):
     '''
     获取1,n(包括n)之间的质数
+    Sieve of Eratosthenes
     '''
     isPrime = [1]*(n+1)
     for i in range(2, int(math.sqrt(n))+1):
         if isPrime[i]:
             for j in range(i**2, n+1, i):
                 isPrime[j] = 0
+                print j
     return [isPrime[i]*i for i in range(n+1) if isPrime[i]*i != 0]
 
 if __name__ == '__main__':
