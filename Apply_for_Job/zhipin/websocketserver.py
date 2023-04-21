eb69557ed4c3ac5b21d00c7065c884651d60c059
YoTro <zhongjin95@gmail.com>
2023-04-21 17:05:58 +0800

    Update websocketserver.py

--- Apply_for_Job/zhipin/websocketserver.py
+++ Apply_for_Job/zhipin/websocketserver.py
@@ -1,21 +1,41 @@
 import asyncio
 import websockets
 import urllib.parse
-async def hello(websocket, path):
-    name = await websocket.recv()
-    if name == 'refresh':
-        await websocket.send('refresh')
-        print(f"Received {name}")
-    else:
-        zp_stoken = urllib.parse.quote(name)
-        with open("./zp_stoken.txt", "w") as f:
-            f.write(zp_stoken)
-        print(f"Received {zp_stoken}")
+import re
+
+async def zp_stoken(websocket, path):
+    try:
+        # 迭代接收来自客户端的消息
+        async for message in websocket:
+            response = f"Received message: {message}"
+            print(response)
+            # 在这里处理接收到的消息
+            if(len(message)>60 and re.match('[a-zA-Z0-9]+',message)):
+                # 将消息写入文件
+                __zp_stoken__ = urllib.parse.quote(message)
+                with open("./zp_stoken.txt", "w") as f:
+                    f.write(__zp_stoken__)
+                # 发送回复消息
+                response = __zp_stoken__
+            # 如果接收到的消息是 'refresh'，则回复同样的消息
+            if(message == 'refresh'):
+                response = message
+            # 发送回复消息到客户端
+            await websocket.send(response)
+            print(f"Sent message: {response}")
+    except websockets.exceptions.ConnectionClosedError as e:
+        print(f"Connection closed with error: {e}")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        await websocket.close()
 
 async def start_server():
-    async with websockets.serve(hello, "localhost", 8765):
-        await asyncio.Future()  # keep server running
+    # 启动 WebSocket 服务器
+    async with websockets.serve(zp_stoken, "localhost", 8765):
+        # 保持服务器运行
+        await asyncio.Future()
 
-# 启动 WebSocket 服务器并开始事件循环
+# 如果作为脚本运行，启动 WebSocket 服务器并开始事件循环
 if __name__ == '__main__':
     asyncio.run(start_server())
