af4205fb9c973316fc623a075686360af556ebd7
YoTro <zhongjin95@gmail.com>
2023-04-26 16:10:55 +0800

    Update main.py

--- Apply_for_Job/zhipin/main.py
+++ Apply_for_Job/zhipin/main.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     scene = "1"#场景
     queryjob = "亚马逊运营"#岗位关键词
     city = "深圳"#城市
-    counties = {"龙岗区":["岗头"]}
     experience = ""#工作经验
     payType = ""#工资结算周期
     partTime = ""#兼职时间
@@ -26,28 +25,28 @@ if __name__ == '__main__':
     data = {'zpData':{'jobList':[]}}
     boss = zhipin()
     citycode = boss.__citycode__(city)
-    countiescode = boss.__businessDistrictcode__(citycode, counties)
-    print("-------开始抓取-------\n")
-    # 最长10页数据
-    for i in tqdm(range(1, 11)):
-        try:
-            response = boss.Search_jobs(scene, queryjob, citycode, experience, payType, partTime, degree, industry, scale, stage, position, jobType, salary, multiBusinessDistrict, multiSubway, str(i), pageSize)
-            if (response['zpData']['hasMore']==False and response['zpData']['resCount']<=i*30):
-                break
-            data['zpData']['jobList']+=response['zpData']['jobList']
-            if(response['zpData']['jobList'] == []):
-                print("抓取失败:第{}页".format(i))
-        except Exception as e:
-            print(e)#
-    boss.Save_To_Excel(data)
-#    login_zhipin(phonenumber)
-#    with open("./jobs.csv", "a", newline="", encoding="utf-8") as f:
-#        writer = csv.writer(f)
-#        writer.writerow(["职位", "公司", "工资", "工作地点", "详情页链接"])
-#    for page in range(1, 2):
-#        print("当前页数:{}".format(page))
-#        html,cookies = __search(queryjob, city, page)
-#        datas = match_job(html)
-#        save_To_csv(datas)
+    counties = boss.getbusinessDistrict(citycode)
+    print(counties)
+    for c in counties:
+        for p in counties[c]:
+            saved_path = './zhipinjobs_{}.csv'.format(p)
+            print(saved_path)
+            countiescode = boss.__businessDistrictcode__(citycode, {c:[p]})
+            print("-------开始抓取-------\n")
+            # 最长10页数据
+            for i in tqdm(range(1, 11)):
+                try:
+                    response = boss.Search_jobs(scene, queryjob, citycode, experience, payType, partTime, degree, industry, scale, stage, position, jobType, salary, multiBusinessDistrict, multiSubway, str(i), pageSize)
+                    # 不够10页直接停止
+                    if (response['zpData']['hasMore']==False and response['zpData']['resCount']<=i*30):
+                        break
+                    data['zpData']['jobList']+=response['zpData']['jobList']
+                    # 为空抓取失败
+                    if(response['zpData']['jobList'] == []):
+                        print("抓取失败:第{}页".format(i))
+                except Exception as e:
+                    pass
+            boss.Save_To_Excel(data, saved_path)
+
 
 
